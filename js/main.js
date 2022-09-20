83286cd5c0332f8dd5f562044607c07eb6b12eeb
joscord <josecorderodev@gmail.com>
2022-09-20 19:40:23 -0300

    add: modified html file. Added API constants and html elements to js file

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// API CONSTANTS
+const API_KEY = 'GEJ0G4itd91z4CZDGfzcwTh1ybhX4dvk';
+const BASE_URL = 'https://api.giphy.com/v1/gifs/';
+
+// HTML ELEMENTS
+searchInput = document.getElementById('search');
+gifsDiv = document.getElementById('gifs');
