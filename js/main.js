f58974d249aabd98b718a044c27337d4e4492b13
joscord <josecorderodev@gmail.com>
2022-09-20 19:47:03 -0300

    add: defined fetchGifs, displayGifs and fetchTrendingGifs

--- js/main.js
+++ js/main.js
@@ -5,3 +5,35 @@ const BASE_URL = 'https://api.giphy.com/v1/gifs/';
 // HTML ELEMENTS
 searchInput = document.getElementById('search');
 gifsDiv = document.getElementById('gifs');
+
+// FUNCTIONS
+const fetchGifs = async (query = '') {
+    let endpoint = '';
+    if (query) {
+        endpoint = `${BASE_URL}search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+    } else {
+        endpoint = `${BASE_URL}trending?api_key=${API_KEY}&limit=25&rating=g`
+    }
+    const response = await fetch(endpoint)
+    const data = await response.json()
+    return data
+}
+
+const displayGifs = async () => {
+    gifsDiv.innerHTML = ''
+    gifs = JSON.parse(localStorage.getItem('gifs'))
+    gifs.data.map(gif => {
+		const gifImg = document.createElement('img');
+		gifImg.src = gif.images.original.webp;
+		main.appendChild(gifImg);
+	});
+}
+
+const fetchTrendingGifs = async () => {
+    const gifs = await fetchGifs()
+    localStorage.setItem('gifs', gifs)
+    displayGifs()
+}
+
+window.addEventListener('load', getTrendingGifs)
+
